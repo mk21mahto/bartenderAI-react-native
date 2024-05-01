6b98ad18723ae1751fbae65f0a07b12403fcf9f2
Manish Kumar <manishmahto2196@gmail.com>
2024-05-01 22:59:54 +0530

    first commit

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,21 @@
 import "../global.css";
-import { Slot } from "expo-router";
+import { Stack } from "expo-router";
 
 export default function Layout() {
-  return <Slot />;
+  return (
+    <Stack
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: "#f4511e",
+        },
+        headerTintColor: "#fff",
+        headerTitleStyle: {
+          fontWeight: "bold",
+        },
+      }}
+    >
+      {/* Optionally configure static options outside the route.*/}
+      <Stack.Screen name="index" options={{}} />
+    </Stack>
+  );
 }
